448118f23994ebe60e3f7abd349024f57b4900ce
zyc-2001 <145514227+zyc-2001@users.noreply.github.com>
2024-04-23 19:14:32 -0400

    Update KNN.py

--- models/KNN.py
+++ models/KNN.py
@@ -1,6 +1,6 @@
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.metrics import accuracy_score
-
+from sklearn.metrics import accuracy_score,f1_score
+import numpy as np
 class KNN_classifier:
     def __init__(self, n_neighbors=5, weights='uniform'):
         self.model = KNeighborsClassifier(n_neighbors=n_neighbors, weights=weights)
@@ -10,5 +10,14 @@ class KNN_classifier:
         
     def predict(self, test_X):
         return self.model.predict(test_X)
+    
+    def evaluate(self, test_X, test_y):
+        predictions = self.predict(test_X)
+        
+        accuracy = np.mean(predictions == test_y)
+        f1 = f1_score(test_y, predictions, average='weighted')
+        
+        return accuracy, f1
+    
     
 
